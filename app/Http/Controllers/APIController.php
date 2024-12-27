cce1f78af67223ffc9f6b43c8a69cd85172ae948
Cybernetics <mail@cybernetics.me>
2024-12-27 11:23:53 +0530

    small updates

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -156,7 +156,7 @@ class APIController extends Controller
 
     function saveOrder(Request $request)
     {
-        $items = json_decode($request->items);
+        $items = json_decode(json_encode($request->items));
         return response()->json([
             'status' => true,
             'items' => $items[0],
