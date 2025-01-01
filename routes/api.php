5ad5ed3120e770558aa090c6e55d2720fc8ba9ce
Cybernetics <mail@cybernetics.me>
2025-01-01 10:54:15 +0530

    small updates

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\APIController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
 
 Route::prefix('user')->controller(APIController::class)->group(function () {
     Route::post('/auth', 'getAuthUser')->name('get.auth.user');
