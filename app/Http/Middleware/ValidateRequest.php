942c0d4a81aa8f98d974bba311414b794975a7be
Cybernetics <mail@cybernetics.me>
2025-01-22 09:02:23 +0530

    small updates

--- app/Http/Middleware/ValidateRequest.php
+++ app/Http/Middleware/ValidateRequest.php
@@ -19,7 +19,7 @@ class ValidateRequest
         $headers = collect($request->header())->transform(function ($item) {
             return $item[0];
         });
-        dd($request);
+        dd($request->url());
         die;
         if ($request->ajax()) {
             $token = Config::get('myconfig.authkey');
