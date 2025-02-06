7998c43987de2cb1e5ad1640ae71a7882d8ac0a9
Cybernetics <mail@cybernetics.me>
2025-02-06 11:22:44 +0530

    small updates

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -384,7 +384,7 @@ class APIController extends Controller
     function getResult(Request $request)
     {
         $result = Result::whereDate('play_date', $request->json('play_date'))->where('play_id', $request->json('play_id'))->first();
-        sort($result->toArra());
+        sort($result->toArray());
         return response()->json([
             'status' => true,
             'result' => $result,
