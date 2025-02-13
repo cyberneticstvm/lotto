7abf8295a204cc224407d2c6da1536703da5253a
Cybernetics <mail@cybernetics.me>
2025-02-13 16:14:52 +0530

    small updates

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -467,7 +467,7 @@ class APIController extends Controller
         if ($request->json('role') == 'leader'):
             $ratecol = 'orders.leader_rate';
         endif;
-        $data = Order::selectRaw("SUM(orders.ticket_count) AS ticket_count, $ratecol * orders.ticket_count AS total")->whereBetween('play_date', [$request->json('from_date'), $request->json('to_date')])->when($request->json('play_id') > 0, function ($q) use ($request) {
+        $data = Order::selectRaw("orders.id, SUM(orders.ticket_count) AS ticket_count, $ratecol * orders.ticket_count AS total")->whereBetween('play_date', [$request->json('from_date'), $request->json('to_date')])->when($request->json('play_id') > 0, function ($q) use ($request) {
             return $q->where('play_id', $request->json('play_id'));
         })->when($request->json('ticket_id') > 0, function ($q) use ($request) {
             return $q->where('ticket_id', $request->json('ticket_id'));
@@ -485,7 +485,7 @@ class APIController extends Controller
             return $q->whereIn('ticket_id', [3, 4, 5]);
         })->when($request->json('option') == 3, function ($q) {
             return $q->whereIn('ticket_id', [1, 2]);
-        })->get();
+        })->groupBy('orders.id')->get();
         return response()->json([
             'status' => true,
             'count' => $data->sum('ticket_count'),
