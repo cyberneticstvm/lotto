18f8781e4cb39fe79fc9b41043970eaeed1ab9d2
Cybernetics <mail@cybernetics.me>
2025-02-18 22:28:38 +0530

    small updates

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -560,7 +560,7 @@ class APIController extends Controller
 
     function getBillsForDelete(Request $request)
     {
-        $bills = Order::leftJoin('plays AS p', 'orders.play_id', 'p.id')->whereDate('orders.play_date', '>=', Carbon::today())->whereTime('p.locked_from', '>', Carbon::now())->select('orders.id', 'orders.play_date', 'orders.ticket_name', 'orders.bill_number', 'p.name')->when($request->json('role') == 'user', function ($q) use ($request) {
+        $bills = Order::leftJoin('plays AS p', 'orders.play_id', 'p.id')->whereDate('orders.play_date', '>=', Carbon::today())->whereTime('p.locked_from', '>', Carbon::now())->select('orders.id', 'orders.play_date', 'orders.ticket_name', 'orders.ticket_number', 'orders.bill_number', 'p.name')->when($request->json('role') == 'user', function ($q) use ($request) {
             return $q->where('user_id', $request->json('user_id'));
         })->when($request->json('role') == 'leader', function ($q) use ($request) {
             return $q->where('parent_id', $request->json('user_id'));
